78626c4a41bebcab30638dd40977049bd049a6f6
nvtrakhmawati <48206190+nvtrakhmawati@users.noreply.github.com>
2019-03-21 10:44:19 +0700

    Update tukarbesarkecil.js

--- rabu/tukarbesarkecil.js
+++ rabu/tukarbesarkecil.js
@@ -1,45 +1,24 @@
 function tukarBesarKecil(kalimat) {
-    var out =''
-    var spasi = ' '
+    var out = ''
     var besar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     var kecil = 'abcdefghijklmnopqrstuvwxyz'
 
     for(var i=0; i<kalimat.length; i++){
+        var found = false
         for( var j=0; j<besar.length; j++){
             if (kalimat[i] == besar[j]){
                 out += kecil[j]
+                found = true;
             } else if(kalimat[i] == kecil[j]){
                 out += besar[j]
+                found = true;
             } 
         }
-        if (kalimat[i] == spasi){
-            out += spasi
-        } else if (kalimat[i] == '0'){
-            out += '0'
-        } else if (kalimat[i] == '1'){
-            out += '1'
-        } else if (kalimat[i] == '2'){
-            out += '2'
-        } else if (kalimat[i] == '3'){
-            out += '3'
-        } else if (kalimat[i] == '4'){
-            out += '4'
-        } else if (kalimat[i] == '5'){
-            out += '5'
-        } else if (kalimat[i] == '6'){
-            out += '6'
-        } else if (kalimat[i] == '7'){
-            out += '7'
-        } else if (kalimat[i] == '8'){
-            out += '8'
-        } else if (kalimat[i] == '9'){
-            out += '9'
-        } else if (kalimat[i] == '-'){
-            out += '-'
-        } else if (kalimat[i] == '!'){
-            out += '!'
-        }     
+        if (found == false){
+            out  +=kalimat[i]
+        }
     }
+
     return out
 }
     
