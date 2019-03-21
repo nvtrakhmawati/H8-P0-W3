2d1c96c33fa03bea5f49f8c06baddfaf405f6b61
nvtrakhmawati <48206190+nvtrakhmawati@users.noreply.github.com>
2019-03-21 10:43:43 +0700

    Update ubahhuruf.js

--- selasa/ubahhuruf.js
+++ selasa/ubahhuruf.js
@@ -4,10 +4,13 @@ function ubahHuruf(kata) {
 
     for (var i=0; i<kata.length; i++){
         for ( var j=0; j<alfabet.length; j++){
-            if (kata[i] == alfabet[j]){
+            if (kata[i] == alfabet[j] && kata[i] !== 'z'){
                 after += alfabet[j+1]
+            } 
+            if (kata[i] == alfabet[j] && kata[i] == 'z'){
+                after += alfabet[0]
             }
-        }
+        } 
     }
     return after
 }
